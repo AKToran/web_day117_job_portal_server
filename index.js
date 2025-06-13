c150b21b525c8858af8cfcec1ed9ff5471bab543
AKToran <aktoran60@gmail.com>
2025-06-13 11:35:05 +0600

    specific job with id

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -28,6 +28,13 @@ async function run() {
     app.get('/jobs', async(req, res)=>{
       const jobs = await jobsCollection.find().toArray();
       res.send(jobs);
+    })
+
+    app.get('/jobs/:id', async(req, res)=>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id) };
+      const job = await jobsCollection.findOne(query);
+      res.send(job);
     })
 
 
